3b9a28c4bdbac3fdc656ba11ecfb9a2c500a4b5d
Your Name <you@example.com>
2017-02-24 14:38:51 -0500

    Finishing up validations

--- script/main.js
+++ script/main.js
@@ -12,8 +12,9 @@ class checkForm {
 	this.input = input;
     this.type = type;
     this.errors = [];
+    // console.log(this.errors);
+    // console.log(this.input);
 	}
-
 
 	populateErrors(mess){
 		this.errors.push(mess);
@@ -21,6 +22,29 @@ class checkForm {
 
 	getError(){
 		var status = this.input.validity
+
+	if(this.type == 'password'){
+    	if (!this.input.value.match(/[A-Z]/g)) {
+      		this.populateErrors('Must contain at least one uppercase letter')
+    	}
+    	if(!this.input.value.match(/[a-z]/g)){
+    		this.populateErrors('Must contain at least one lowercase letter')
+    	}
+    	if(!this.input.value.match(/\d/g)){
+    		this.populateErrors('Must contain at least one number')
+    	}
+    	if(!this.input.value.match(/[a-zA-Z0-9]{8,}/g)){
+    		this.populateErrors('Entry is too short must be 8 character or more')
+    	}
+    }
+    if(this.type == 'email'){
+    	if(!this.input.value.match(/\@/g)){
+    		this.populateErrors('Must have a @ symbol in the email')
+    	}
+    	if(!this.input.value.match(/\./g)){
+    		this.populateErrors('Email must folow this format "example@service.com"')
+    	}
+    }	
 	
 	if (status.typeMismatch) {
       this.populateErrors('Entry does not match the field type');
@@ -29,20 +53,9 @@ class checkForm {
     if (status.tooLong) {
       this.populateErrors('Entry is too long');
     }
-
-    if (status.tooShort) {
-      this.populateErrors('Entry is too short');
-    }
-    if (!this.input.value.match(/[A-Z]/g)) {
-      this.populateErrors('Must contain at least one uppercase letter')
-    }
-
 	    return this.errors;
 	}
-
-
 }
-
 submit.addEventListener('click', function(event){
 		event.preventDefault();
 
@@ -52,7 +65,23 @@ submit.addEventListener('click', function(event){
 		var errorName = checkName.getError();
 		var checkEmail = new checkForm(email, "email");
 		var errorEmail = checkEmail.getError();
-		// console.log(errorMessages);
-		console.log(errorEmail);
+		var checkPhone = new checkForm(tel, "number");
+		var errorTel = checkPhone.getError();
+		// console.log(checkForm.errors);
+		if (errorMessages.length > 0) {
+    		errorMessages.forEach( (err) => {
+      		pass.insertAdjacentHTML('afterend', '<p class="error">' + err + '</p>')
+    		})
 
+    	}
+    	if (errorName.length > 0){
+    		errorName.forEach( (e) => {
+    		 fullname.insertAdjacentHTML('afterend', '<p class="error">' + e + '</p>')
+    		})
+    	}
+    	if (errorEmail.length > 0){
+    		errorEmail.forEach( (er) =>{
+    			email.insertAdjacentHTML('afterend', '<p class="error">' + er + '</p>')
+    		})
+    	}
 	})
